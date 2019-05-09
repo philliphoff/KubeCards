a622dba67c5f82496e093b20fede242961cf735f
Phillip Hoff <phillip@orst.edu>
2019-05-09 14:44:24 -0700

    Move script into Helm charts.

--- scripts/https-setup.sh
+++ scripts/https-setup.sh
@@ -3,20 +3,14 @@
 # chart in the next step for `release-0.8` of cert-manager:
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
 
-# Create cert-manager namespace
-kubectl create namespace cert-manager
-
-## IMPORTANT: if the cert-manager namespace **already exists**, you MUST ensure
-## it has an additional label on it in order for the deployment to succeed
-kubectl label namespace cert-manager certmanager.k8s.io/disable-validation="true"
+# Create cert-manager namespace and issuer
+helm install ../charts/kube-cards-cert-manager
 
 ## Add the Jetstack Helm repository
 helm repo add jetstack https://charts.jetstack.io
+
 ## Updating the repo just incase it already existed
 helm repo update
 
 ## Install the cert-manager helm chart
 helm install --name cert-manager --namespace cert-manager jetstack/cert-manager
-
-# Create issuer
-kubectl apply -f ../charts/cluster-issuer.yaml
--- scripts/kubernetes-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a namespace for your ingress resources
-kubectl create namespace ingress-basic
-
-# Use Helm to deploy an NGINX ingress controller (to same namespace as ingress resource)
-helm install stable/nginx-ingress --set controller.replicaCount=2
