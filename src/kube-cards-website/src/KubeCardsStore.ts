d3e5196cabe71999b3e9a7aa2119d1f89bf52288
Phillip Hoff <phillip@orst.edu>
2019-05-14 11:15:41 -0700

    Scaffold redux-thunk for async actions.

--- src/kube-cards-website/src/KubeCardsStore.ts
+++ src/kube-cards-website/src/KubeCardsStore.ts
@@ -1,9 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 import rootReducer from './reducers/RootReducer';
 
 export default createStore(
     rootReducer,
     {} /* Initial state */,
-    composeWithDevTools());
+    composeWithDevTools(applyMiddleware(thunk)));
