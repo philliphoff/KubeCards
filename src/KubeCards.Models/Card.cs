0a705041269cc83adf8fd1f5922cbfb633556324
Steven Green <sgreen@microsoft.com>
2019-05-15 19:42:40 -0700

    Initial CardsInventoryService implementation

---
+++ src/KubeCards.Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KubeCards.Models
+{
+    public class Card
+    {
+        public string CardId { get; set; }
+        public int CardValue { get; set; }
+        public DateTime CreatedDateTimeUtc { get; set; }
+    }
+}
