81f64767776123a61ab9b6e0942859cbb4914476
Phillip Hoff <phillip@orst.edu>
2019-05-17 12:59:20 -0700

    Add progress indicator during login.

--- src/kube-cards-website/src/KubeCardsAuth.tsx
+++ src/kube-cards-website/src/KubeCardsAuth.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStyles, WithStyles, withStyles } from '@material-ui/core/styles';
+import { createStyles, WithStyles, withStyles, Theme, StyleRulesCallback } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import { userAuthMsalLogin } from './actions/UserAuthActions';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const styles = createStyles({
+const styles: StyleRulesCallback = (theme: Theme) => ({
+    wrapper: {
+        margin: theme.spacing.unit,
+        position: 'relative'
+    },
+    buttonProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12
+    }
 });
 
 interface Props extends WithStyles<typeof styles> {
@@ -23,11 +37,12 @@ const KubeCardsAuth: React.FunctionComponent<Props> = props => {
         return <Avatar>{userInitials}</Avatar>;
     }
     else {
-        const { onLogin } = props;
+        const { classes, onLogin } = props;
 
         return (
-            <div>
+            <div className={classes.wrapper}>
                 <Button color="inherit" onClick={onLogin}>Login</Button>
+                { state === 'loggingIn' && <CircularProgress size={24} className={classes.buttonProgress} /> }
             </div>
         );
     }
