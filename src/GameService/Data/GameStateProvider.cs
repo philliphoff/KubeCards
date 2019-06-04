d13f04f55885a5ee2cd61d8b541de479708773b6
Steven Green <sgreen@microsoft.com>
2019-06-04 13:32:55 -0700

    wip

---
+++ src/GameService/Data/GameStateProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameService.Data
+{
+    public class GameStateProvider : IGameStateProvider
+    {
+        private readonly IConfiguration configuration;
+
+        public GameStateProvider(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+    }
+}
