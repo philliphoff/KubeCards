a596eac94ee69b8c8c342e8575afa89dee2c8378
Phillip M. Hoff <phoff@Phillips-MacBook-Pro-2018.local>
2019-06-12 10:33:10 -0700

    Sketch games service API.

--- src/kube-cards-website/src/Models.ts
+++ src/kube-cards-website/src/Models.ts
@@ -7,3 +7,24 @@ export interface IDeck {
     cards: ICard[];
     id?: string;
 }
+
+export interface IPlayer {
+    displayName: string;
+    userId: string;
+    handCards: ICard[];
+    playedCards: ICard[];
+}
+
+export interface IGameAction {
+    description: string;
+    actionDateTimeUtc: string;
+}
+
+export interface IGameState {
+    gameId: string;
+    player1: IPlayer;
+    player2: IPlayer;
+    history: IGameAction[];
+    nextPlayerUserId: string;
+    lastUpdatedDateTimeUtc: string;
+}
